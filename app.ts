c870b2848eb502699b015008102ea1e57ea70b42
Mawuli87 <yawomessie@gmail.com>
2024-09-15 13:46:00 +0000

    Login and registration with sending of token

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -18,6 +20,9 @@ app.use(
   })
 );
 
+// routes
+app.use("/api/v1", userRouter);
+
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
@@ -32,3 +37,7 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+// middleware calls
+//app.use(limiter);
+app.use(ErrorMiddleware);
